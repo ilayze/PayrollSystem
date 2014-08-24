1b9a37821d9c8c86250384beacc95c2954eb78ac
ilay zeidman <ilayndeganit@gmail.com>
2014-08-24 13:01:01 +0300

    add salaried employee

---
+++ PayrollSystem/Logic/Transactions/AddSalariedEmployee.cs
@@ -0,0 +1,34 @@
+﻿using Logic.Payment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Transactions
+{
+    public class AddSalariedEmployee:Transaction
+    {
+        private int empId;
+        private string _name;
+        private string _address;
+        private double _salary;
+
+        public AddSalariedEmployee(int empId, string name, string address, double salary)
+        {
+            // TODO: Complete member initialization
+            this.empId = empId;
+            this._name = name;
+            this._address = address;
+            this._salary = salary;
+
+        }
+        public void Execute()
+        {
+            SalariedClassification sc = new SalariedClassification(_salary);
+
+            Employee e = new Employee(_name, _address, sc);
+            PayrollDatabase.AddEmployee(empId, e);
+        }
+    }
+}
