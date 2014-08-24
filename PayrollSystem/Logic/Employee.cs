ca0ca7ade54d6a1c0c98ff9bb6bd06a58f19d7f6
ilay zeidman <ilayndeganit@gmail.com>
2014-08-24 09:02:30 +0300

    make addSalaried employee unit test pass

---
+++ PayrollSystem/Logic/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Employee
+    {
+        private string _name;
+        private string _address;
+        private double _salary;
+
+        public string Name { get { return _name; } }
+
+        public Employee(string _name, string _address, double _salary)
+        {
+            // TODO: Complete member initialization
+            this._name = _name;
+            this._address = _address;
+            this._salary = _salary;
+        }
+        
+    }
+}
---
+++ PayrollSystem/Logic/PayrollDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class PayrollDatabase
+    {
+        static Dictionary<int, Employee> _employees = new Dictionary<int, Employee>();
+
+        public static void AddEmployee(int id,Employee e)
+        {
+            _employees[id] = e;
+        }
+
+        public static Employee GetEmployee(int empId)
+        {
+            if(_employees.ContainsKey(empId))
+                return _employees[empId];
+            return null;
+        }
+    }
+}
