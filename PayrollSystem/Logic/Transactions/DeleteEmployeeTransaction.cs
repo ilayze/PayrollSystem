6c69a2a25e667063c7c42773a4a81ab6ea72d894
ilay zeidman <ilayndeganit@gmail.com>
2014-08-25 15:08:58 +0300

    add delete employee transaction

---
+++ PayrollSystem/Logic/Transactions/DeleteEmployeeTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Transactions
+{
+    public class DeleteEmployeeTransaction:Transaction
+    {
+        private int empId;
+
+        public DeleteEmployeeTransaction(int empId)
+        {
+            // TODO: Complete member initialization
+            this.empId = empId;
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
