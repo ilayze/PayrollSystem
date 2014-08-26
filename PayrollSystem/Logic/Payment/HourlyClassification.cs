7d0a3fcafcfeb8a38ff16c954e462a48081c77af
ilay zeidman <ilayndeganit@gmail.com>
2014-08-26 14:48:11 +0300

    added time card transaction

--- PayrollSystem/Logic/Payment/HourlyClassification.cs
+++ PayrollSystem/Logic/Payment/HourlyClassification.cs
@@ -9,13 +9,26 @@ namespace Logic.Payment
     public class HourlyClassification: PaymentClassification
     {
         private double _paymentPerHour;
+        private Dictionary<DateTime, TimeCard> timeCards;
 
         public double PaymentPerHour { get { return _paymentPerHour; } }
 
         public HourlyClassification(double paymentPerHour)
         {
             this._paymentPerHour = paymentPerHour;
+            timeCards = new Dictionary<DateTime, TimeCard>();
         }
 
+
+        public TimeCard GetTimeCard(DateTime dateTime)
+        {
+            return timeCards[dateTime];
+        }
+
+        internal void AddTimeCard(DateTime dateTime, double hours)
+        {
+            TimeCard tc = new TimeCard(dateTime, hours);
+            timeCards[dateTime] = tc;
+        }
     }
 }
